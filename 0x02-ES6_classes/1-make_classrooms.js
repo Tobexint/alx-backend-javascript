d6fd6299f9d6a640e9f226814610bfde3e9885e8
tobexint <tobexint@gmail.com>
2023-01-26 17:55:31 +0000

    room

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,10 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-   const room = [];
-   room.push(new ClassRoom(19));
-   room.push(new ClassRoom(20));
-   room.push(new ClassRoom(34));
+  const room = [];
+  room.push(new ClassRoom(19));
+  room.push(new ClassRoom(20));
+  room.push(new ClassRoom(34));
 
-   return room;
+  return room;
 }
