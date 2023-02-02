ba753781b0be983642c6c8ee2e06a832d866e25b
tobexint <tobexint@gmail.com>
2023-02-02 03:59:22 +0000

    groceries

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+function groceriesList() {
+  const mygroceries = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+
+  return new Map(mygroceries);
+}
+
+export default groceriesList;
