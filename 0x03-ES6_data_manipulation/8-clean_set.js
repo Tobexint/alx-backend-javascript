fd59a187883064997850bd612ecfefcef7117059
tobexint <tobexint@gmail.com>
2023-02-02 03:56:16 +0000

    elem

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+function cleanSet(set, startString) {
+  const newList = [];
+  if (startString === '' || typeof startString !== 'string') {
+    return '';
+  }
+  for (const elem of set) {
+    if ((typeof elem === 'string') && (elem.startsWith(startString))) {
+      newList.push(elem.slice(startString.length));
+    }
+  }
+  return newList.join('-');
+}
+
+export default cleanSet;
